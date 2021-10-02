3339b481b4b1858e7e1eab37bb1a11bfc6e3f9b2
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2021-10-02 15:49:06 +0800

    Add autochecker

--- checker.js
+++ checker.js
@@ -1,31 +1,72 @@
 const fs = require("fs");
-const core = require("@actions/core");
 
-function validInput(parsed) {
+function validInput(parsed, p_puzzle) {
   if (parsed.length != 81) return false;
   if (parsed.replace(/([0-9])+/g, "")) return false;
+  // Check the map match original map (so you cant just put your own map)
+  console.log(parsed);
+  console.log(p_puzzle);
+  for (var i = 0; i < p_puzzle.length; i++) {
+    if (!isNaN(p_puzzle[i])) {
+      if (parsed[i] !== p_puzzle[i]) {
+        console.log(parsed[i], p_puzzle[i], i);
+        return false;
+      }
+    }
+  }
   return true;
 }
 
-function validAnswer(parsed) {
+function validAnswer(parsed, p_puzzle) {
   // Check proper sudoku ans
-  // Check the map match original map (so you cant just put your own map)
+  //check row
+  for (var i = 0; i < 9; i++) {
+    var arr = [...parsed.substring(i * 9, (i * 9) + 9)];
+    var check = arr.filter((item, index) => arr.indexOf(item) != index);
+    if (check.length !== 0) {return false}
+  }
+
+  //check column
+  for (var i = 0; i < 9; i++) {
+    var arr = [];
+    for (var j = 0; j < 9; j++) {
+      arr.push(parsed[(j * 9) + i]);
+    }
+    var check = arr.filter((item, index) => arr.indexOf(item) != index);
+    if (check.length !== 0) {return false}
+  }
+
+  //check box
+  [0, 3, 6, 27, 30, 33, 54, 57, 60].forEach((item) => {
+    var arr = [];
+    for (var i = item; i < (item + 3); i++) {
+      arr.push(parsed[i], parsed[i + 9], parsed[i + 18]);
+    }
+    var check = arr.filter((item, index) => arr.indexOf(item) != index);
+    if (check.length !== 0) {return false}
+  })
+  return true;
 }
 
-function checkAnswer(answer) {
+function checkAnswer(answer, puzzle) {
   const parsed = answer.replace(/([\|\-\+\s])+/g, "");
-  console.log(parsed);
-  if (!validInput(parsed)) throw Error("Invalid Input");
+  const p_puzzle = puzzle.replace(/([\|\-\+\s])+/g, "");
+  if (!validInput(parsed, p_puzzle)) throw Error("Invalid Input");
   if (validAnswer(parsed)) console.log("✅ Solution Passed!");
-  else core.error("❌ Solution Failed!");
+  else {
+    console.error("❌ Solution Failed!");
+    throw Error("Incorrect solution");
+  }
 }
 
 function readFile() {
   try {
     const answer = fs.readFileSync("./solution.txt", "utf8");
-    checkAnswer(answer);
+    var puzzle = fs.readFileSync("./README.md", "utf8");
+    puzzle = puzzle.slice(puzzle.length -256, puzzle.length -3);
+    checkAnswer(answer, puzzle);
   } catch (err) {
-    core.error(err);
+    console.error(err);
   }
 }
 
